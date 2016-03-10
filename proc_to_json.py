79434ea7030ddc13c956d31feae41204973c270a
Sanyam Agarwal <sanyam@iitkgp.ac.in>
2016-03-10 19:40:45 +0530

    Multiple spaces removed from json

--- proc_to_json.py
+++ proc_to_json.py
@@ -7,5 +7,6 @@ for number in data_dict:
     print "doing ", number
     for attr in ['text', 'title']:
         data_dict[number][attr] = ''.join([i if ord(i) < 128 else ' ' for i in data_dict[number][attr]])
+        data_dict[number][attr] = ' '.join(data_dict[number][attr].split())
 
 json.dump(data_dict, open("outputs/data.json", "wb"))
